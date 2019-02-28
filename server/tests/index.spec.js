dc50757247047873cb7876923d2277386dd7e64e
Ndifreke Ekim <ndifreke.ekim@andela.com>
2019-02-28 11:48:33 +0100

    eee

--- server/tests/index.spec.js
+++ server/tests/index.spec.js
@@ -1,3 +1,19 @@
+// import "./models/users.spec";
+// import "./models/favourites.spec";
+// import "./miscellaneous/miscellaneous.spec";
+// import "./oauth/social.auth.spec";
+// import "./models/articles.spec";
+// import "./miscellaneous/emails/email.spec";
+// import "./controller/user/signUp.spec";
+// import "./controller/user/verification.spec";
+// import "./controller/user/signIn.spec";
+// import "./controller/user/profile.spec";
+// import "./controller/user/resetPassword.spec";
+// import "./controller/favorite/favorites.spec";
+// import "./models/notifications.spec";
+// import "./controller/article/article.spec";
+// import "./controller/article/pagination.spec";
+
 import "./models/users.spec";
 import "./models/favourites.spec";
 import "./miscellaneous/miscellaneous.spec";
